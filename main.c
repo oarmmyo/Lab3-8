a0d236533b047e6c1640f219bd98fae83f143e38
Theeranai Nakpradit <9nongarm@gmail.com>
2020-08-16 05:39:01 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) 
+{
+	float a;
+  printf("Enter Real Number : ");
+  scanf("%f",&a);
+  printf("Integer -> %0.0f\n", a);
+  printf("Real Number -> %f\n", a);
+	return 0;
+}
